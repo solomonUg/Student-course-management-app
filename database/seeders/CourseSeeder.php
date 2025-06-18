c01d0483c11276173111d3404bc650162b01824b
SolomonUg <solomonchris700@gmail.com>
2025-06-18 16:27:09 +0100

    set up seeders for course and students

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -4,14 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use  App\Models\Course;
 
-class CoursesSeeder extends Seeder
+class CourseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        Course::factory()->count(20)->create();
     }
 }
